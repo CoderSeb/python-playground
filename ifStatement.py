0cc74ecf8f6fb9ff344b1858516738f8b400049d
Sebastian <sa224ny@student.lnu.se>
2021-08-20 15:58:19 +0200

    Added solution for ifStatement.py

--- ifStatement.py
+++ ifStatement.py
@@ -4,8 +4,11 @@ B = 100
 # Skriv en if-sats som skriver ut 'B är större än A' om så är fallet.
 # Skriv din kod nedan. Du kör programmet genom att skriva: python ifStatement.py
 
-
+if B > A:
+    print("B är större än A")
 # Skriv en else if - sats som skriver ut 'A och B är lika' om så är fallet.
+elif A > B:
+    print("A är större än B")
 
 
 # Som hjälp finns all information här.
