adb0b3a8a7133f519b3c611937ec7b0072a20a8c
Sebastian <sa224ny@student.lnu.se>
2021-08-20 15:46:49 +0200

    Added hello.py exercise

---
+++ hello.py
@@ -0,0 +1,5 @@
+greeting = 'Hello World!'
+
+# Skriv din kod här nedan så att 'Hello World!' skrivs ut i terminalen.
+# Du kör programmet genom att skriva: python hello.py
+# Lösningen finns här: https://www.w3schools.com/python/ref_func_print.asp
