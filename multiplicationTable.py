aded3dc42d588b48629f0a87d824feae10324c5e
Sebastian <sa224ny@student.lnu.se>
2021-08-20 16:01:10 +0200

    Added solution for multiplicationTable.py

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -1,3 +1,7 @@
 # Skriv ett program som skriver ut en multiplikations för 1-12.
 # Här finns halva lösningen: https://www.programiz.com/python-programming/examples/multiplication-table
 # Tips! Kika på nästlade for-loopar
+
+for n in range(1, 13):
+    for i in range(1, 13):
+        print(n, 'x', i, '=', n*i)
