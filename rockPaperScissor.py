daa6908cdd27d623651c79f91d1147582807d640
Sebastian <sa224ny@student.lnu.se>
2021-08-20 15:49:19 +0200

    Added rockPaperScissor.py exercise

---
+++ rockPaperScissor.py
@@ -0,0 +1,7 @@
+# Gör ett spel där du spelar sten, sax, påse mot datorn.
+# I programmering är det viktigt att kunna söka information på nätet så gör ett försök!
+# Dela upp problemet i mindre delar.
+# Förslagsvis, börja med att skapa en input där användaren skriver in en siffra eller text för att välja sten, sax eller påse.
+
+# Ett paket som förenklar utvecklingen är random. Som hjälp är det paketet redan importerat. Läs dock på om hur det används.
+import random
