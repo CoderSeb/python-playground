ee43fa3c7661283bad27c0b990c8f1dcb912b3fe
Sebastian <sa224ny@student.lnu.se>
2021-08-20 15:47:29 +0200

    Added ifStatement.py exercise

---
+++ ifStatement.py
@@ -0,0 +1,12 @@
+A = 50
+B = 100
+
+# Skriv en if-sats som skriver ut 'B är större än A' om så är fallet.
+# Skriv din kod nedan. Du kör programmet genom att skriva: python ifStatement.py
+
+
+# Skriv en else if - sats som skriver ut 'A och B är lika' om så är fallet.
+
+
+# Som hjälp finns all information här.
+# https://www.w3schools.com/python/python_conditions.asp
