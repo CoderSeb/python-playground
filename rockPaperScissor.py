badd2cf859cf2f10f1cc1b08018caa2a05415ae4
Sebastian <sa224ny@student.lnu.se>
2021-08-20 15:51:30 +0200

    Added solution for rockPaperScissor.py

--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -5,3 +5,43 @@
 
 # Ett paket som förenklar utvecklingen är random. Som hjälp är det paketet redan importerat. Läs dock på om hur det används.
 import random
+
+userAction = int(input("Välj ett alternativ: (1)-Sten, (2)-Sax eller (3)-Påse: "))
+possibleActions = [1, 2, 3]
+computerAction = random.choice(possibleActions)
+
+
+def convertAction(action):
+    if action == 1:
+        action = "Sten"
+    elif action == 2:
+        action = "Sax"
+    else:
+        action = "Påse"
+    return action
+
+
+print(f"\nDu valde {convertAction(userAction)}, datorn valde {convertAction(computerAction)}.\n")
+
+
+def checkWinner(usrAct, cprAct):
+    if usrAct == cprAct:
+        print("Det blev lika!")
+    elif usrAct == 1:
+        if cprAct == 2:
+            print("Grattis! Du vann!")
+        else:
+            print("Påse slår sten, du förlorade...")
+    elif usrAct == 2:
+        if cprAct == 3:
+            print("Grattis! Du vann!")
+        else:
+            print("Tyvärr... Sten slår sax!")
+    elif usrAct == 3:
+        if cprAct == 1:
+            print("Grattis! Du vann!")
+        else:
+            print("Aj aj! Sax klipper sönder påsen!")
+
+
+checkWinner(userAction, computerAction)
