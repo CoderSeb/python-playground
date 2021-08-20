70cffb2f0bf29ef950d73b607a9ffeeabccca770
Sebastian <sa224ny@student.lnu.se>
2021-08-20 15:48:07 +0200

    Added leapYear.py exercise

---
+++ leapYear.py
@@ -0,0 +1,3 @@
+# Skriv ett program som skriver ut sammanlagt 20 år och om varje år är skottår (leap year).
+# Här finns halva lösningen: https://www.programiz.com/python-programming/examples/leap-year
+# Tips! Kika på for loopar och funktioner.
