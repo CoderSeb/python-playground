ff9efaf921874796c5da64e8d8e4b57bd226bc6f
Sebastian <sa224ny@student.lnu.se>
2021-08-22 21:01:51 +0200

    Added excercise findCommonElements.py

---
+++ findCommonElements.py
@@ -0,0 +1,18 @@
+# Skriv ett program för att hitta antal lika värden i 2 givna listor.
+array1 = [1, 2, 3, 4]
+array2 = [1, 2, 3, 5]
+array3 = [11, 22, 33, 44]
+
+"""
+Funktionen checkSameElements tar in 2 listor (arrayer) vars värden
+jämförs och sedan returneras antalet lika värden i de båda listorna.
+"""
+
+
+def checkSameElements(arr1, arr2):
+    # Din kod här
+
+
+print(checkSameElements(array1, array1))
+print(checkSameElements(array1, array2))
+print(checkSameElements(array1, array3))
