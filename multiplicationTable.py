6c503f6dea0aef5350aa3d7059abdcb10598e516
Sebastian Åkerblom <26320733+CoderSeb@users.noreply.github.com>
2021-08-23 12:00:22 +0200

    Update multiplicationTable.py

--- multiplicationTable.py
+++ multiplicationTable.py
@@ -1,3 +1,3 @@
-# Skriv ett program som skriver ut en multiplikations för 1-12.
+# Skriv ett program som skriver ut en multiplikationstabell för 1-12.
 # Här finns halva lösningen: https://www.programiz.com/python-programming/examples/multiplication-table
 # Tips! Kika på nästlade for-loopar
